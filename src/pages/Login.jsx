cf0f6a9bc96223c0673cdbb375802e9785101b8a
Lao <lautaro.s.larragueta@gmail.com>
2022-08-13 16:05:20 +0100

    validacion de registro y login y le di un hover al button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,100 +1,169 @@
-import React from 'react'
-import  { useState } from 'react'
-import { Grid, Container, Paper,Typography,TextField,Button, CssBaseline } from '@mui/material'
-import SpaSharpIcon from '@mui/icons-material/SpaSharp';
-
+import React from "react";
+import { useState } from "react";
+import {
+  Grid,
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  CssBaseline,
+} from "@mui/material";
+import SpaSharpIcon from "@mui/icons-material/SpaSharp";
 
 const Login = () => {
-    const [body, setBody] = useState({ nickname: '', password: '' })
-  
-    
-	const handleChange = e => {
-		setBody({
-			...body,
-			[e.target.name]: e.target.value
-		})
-	}
+  const [email, setEmail] = useState("");
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [legendEmail, setLegendEmail] = useState("");
 
-	const onSubmit = () => {
-		console.log(body)
-	}
+  const [password, setPassword] = useState("");
+  const [legendPasword, setLegendPassword] = useState("");
+  const [errorPassword, setErrorPassword] = useState(false);
 
-    return (
+  const emailExp = /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
+  const passwordExp = /^[a-z0-9_-]{6,18}$/;
+
+  const emailValidation = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+
+    if (!emailExp.test(email)) {
+      setErrorEmail(() => true);
+      setLegendEmail("por favor escriba su email de manera correcta");
+    } else {
+      setErrorEmail(false);
+      setLegendEmail("ok");
+    }
+  };
+
+  const passwordValidation = (event) => {
+    event.preventDefault();
+    setPassword(event.target.value);
+
+    if (!passwordExp.test(password)) {
+      setErrorPassword(() => true);
+      setLegendPassword("contraseña invalida");
+    } else {
+      setErrorPassword(false);
+      setLegendPassword("contraseña valida");
+    }
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    setEmail(event.target.value);
+    setPassword(event.target.value);
+
+    if (email === "") {
+      setErrorEmail(() => true);
+      setLegendEmail("complete el campo solicitado");
+      alert(" ingrese los datos requeridos para el registro por favor ");
+    } else if (password.length === 0) {
+      setErrorPassword(() => true);
+      setLegendEmail("complete el campo solicitado");
+      alert(" ingrese los datos requeridos para el registro por favor ");
+    } else {
+      console.log({
+        email,
+        password,
+      });
+
+      console.log("ok");
+    }
+  };
+
+  return (
     <div>
-      <Grid container component='main' >
-			<CssBaseline />
-			<Container component={Paper} elevation={5} maxWidth='xs' sx={{ 
-				bgcolor: '#004d40',
-				borderRadius: 2,
-			      }} >
-				<div>
-					<SpaSharpIcon sx={{ 
-						fontSize: "30px", 
-						color: "white", 
-						position:"relative", 
-						top:"85px", 
-						left:'95px'  }}/>
-					<Typography
-					component='h1' 
-					variant='h5' 
-					sx={{textAlign:'center', 
-					     fontSize:'30px', 
-					     color:'white', 
-					     marginTop:'50px', 
-					     marginBottom:'40px',
-						 marginLeft:'30px',
-						 fontFamily:'Roboto' }} > PLANTASIA</Typography>
-					<form >
-						<TextField
-						sx={{
-							marginBottom:'30px',
-							marginTop:'30px',
-							
-						}}
-							fullWidth
-							autoFocus
-							color='primary'
-							margin='normal'
-							variant='standard'
-							label='Email'
-							name='Email'
-							value={body.email}
-							onChange={handleChange}
-						/>
-						<TextField
-							fullWidth
-							autoFocus
-							type='password'
-							color='primary'
-							margin='normal'
-							variant='standard'
-							label='Password'
-							name='password'
-							value={body.password}
-							onChange={handleChange}
-						/>
-						<Button sx={{
-							marginTop:'50px',
-							marginBottom:'100px',
-							bgcolor:'transparent',
-							width:'50%',
-							marginLeft:'100px',
-							fontSize:'20px'
-						}}
-							fullWidth
-							variant='contained'
-							color='primary'
-						
-							onClick={() => onSubmit()}
-						>
-							Iniciar Sesión
-						</Button>
-					</form>
-				</div>
-			</Container>
-		</Grid>
+      <Grid container component="main">
+        <CssBaseline />
+        <Container
+          component={Paper}
+          elevation={5}
+          maxWidth="xs"
+          sx={{
+            bgcolor: "#004d40",
+            borderRadius: 2,
+          }}
+        >
+          <div>
+            <SpaSharpIcon
+              sx={{
+                fontSize: "30px",
+                color: "white",
+                position: "relative",
+                top: "85px",
+                left: "95px",
+              }}
+            />
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{
+                textAlign: "center",
+                fontSize: "30px",
+                color: "white",
+                marginTop: "50px",
+                marginBottom: "40px",
+                marginLeft: "30px",
+                fontFamily: "Roboto",
+              }}
+            >
+              {" "}
+              PLANTASIA
+            </Typography>
+            <form>
+              <TextField
+                sx={{
+                  marginBottom: "30px",
+                  marginTop: "30px",
+                }}
+                fullWidth
+                autoFocus
+                color="primary"
+                margin="normal"
+                variant="standard"
+                label="Email"
+                name="Email"
+                onChange={emailValidation}
+                error={errorEmail}
+                helperText={legendEmail}
+              />
+              <TextField
+                fullWidth
+                autoFocus
+                type="password"
+                color="primary"
+                margin="normal"
+                variant="standard"
+                label="Password"
+                name="password"
+				onChange={passwordValidation}
+				error={errorPassword}
+				helperText={legendPasword}
+              />
+              <Button
+                sx={{
+                  marginTop: "50px",
+                  marginBottom: "100px",
+                  bgcolor: "transparent",
+                  width: "50%",
+                  marginLeft: "100px",
+                  fontSize: "20px",
+                }}
+                fullWidth
+                variant="contained"
+                color="primary"
+				onClick={handleLogin}
+              >
+                Iniciar Sesión
+              </Button>
+            </form>
+          </div>
+        </Container>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,64 +1,196 @@
 import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
 const RegisterForm = () => {
-  const [Email, setEmail] = useState("");
+  const [user, setUser] = useState("");
+  const [errorUser, setErrorUser] = useState(false);
+  const [legendUser, setLegendUser] = useState("");
+
+  const [data, setData] = useState("");
+  const [errorData, setErrorData] = useState(false);
+  const [legendData, setLegendData] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [legendEmail, setLegendEmail] = useState("");
+
   const [password, setPassword] = useState("");
+  const [legendPasword, setLegendPassword] = useState("");
+  const [errorPassword, setErrorPassword] = useState(false);
+
+  const emailExp = /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
+  const passwordExp = /^[a-z0-9_-]{6,18}$/;
+  const userExp = /^[a-z0-9_-]{3,6}$/;
+  const dataExp = /[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/;
+
+  const userNameValidation = (event) => {
+    event.preventDefault();
+    setUser(event.target.value);
+
+    if (!userExp.test(user)) {
+      setErrorUser(true);
+      setLegendUser("por favor escriba un nombre de usuario valido");
+    } else {
+      setErrorUser(false);
+      setLegendUser("ok");
+    }
+  };
+
+  const dataValidation = (event) => {
+    event.preventDefault();
+    setData(event.target.value);
+
+    if (!dataExp.test(data)) {
+      setErrorData(true);
+      setLegendData("por favor escriba su nombre y apellido correspondiente");
+    } else {
+      setErrorData(false);
+      setLegendData("ok");
+    }
+  };
+
+  const emailValidation = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+
+    if (!emailExp.test(email)) {
+      setErrorEmail(true);
+      setLegendEmail("por favor escriba su email de manera correcta");
+    } else {
+      setErrorEmail(false);
+      setLegendEmail("ok");
+    }
+  };
+
+  const passwordValidation = (event) => {
+    event.preventDefault();
+    setPassword(event.target.value);
+
+    if (!passwordExp.test(password)) {
+      setErrorPassword(true);
+      setLegendPassword("contraseña invalida");
+    } else {
+      setErrorPassword(false);
+      setLegendPassword("contraseña valida");
+    }
+  };
 
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log({
-      Email,
-      password,
-    });
-  };
 
-  const handleEmailChange = (event) => {
-    setEmail(() => event.target.value);
-  };
-  const handlePasswordChange = (event) => {
-    setPassword(() => event.target.value);
+    setUser(event.target.value);
+    setData(event.target.value);
+    setEmail(event.target.value);
+    setPassword(event.target.value);
+
+    if (user === "") {
+      
+      setErrorUser(true);
+      setLegendUser("complete el campo solicitado");
+      alert(" ingrese nombre de usuario para el registro por favor ");
+    } else if (data === "") {
+     
+      setErrorData(true);
+      setLegendData("complete el campo solicitado");
+      alert(" ingrese nombre y apellido para el registro por favor ");
+    } else if (email.length === 0) {
+     
+      setErrorEmail(true)
+      setLegendEmail("complete el campo solicitado")
+      alert(" ingrese los datos requeridos para el registro por favor ");
+    } else if (password.length === 0) {
+     
+      setErrorPassword(true);
+      setLegendEmail("complete el campo solicitado")
+      alert(" ingrese los datos requeridos para el registro por favor ");
+    } else {
+      console.log({
+        user,
+        data,
+        email,
+        password,
+      });
+
+      console.log("ok");
+    }
+
+    //condición ? expr1 : expr2
   };
 
   return (
     <div className="form">
-      <h1>REGISTRARSE</h1>
+      <h1>REGISTRO</h1>
       <form>
-        <div>
-          <label>Username: </label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label>Nombre: </label>
-          <input type="text" id="nombre" name="nombre" />
-        </div>
-        <div>
-          <label>Apellido: </label>
-          <input type="text" id="apellido" name="apellido" />
-        </div>
-        <div>
-          <label>Email: </label>
-          <input
-            type="Email"
-            id="email"
-            name="email"
-            value={Email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div>
-          <label>Password: </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button onClick={handleLogin}>Iniciar Sesión</button>
+        <TextField
+          type="text"
+          label="username"
+          name="username"
+          onChange={userNameValidation}
+          error={errorUser}
+          helperText={legendUser}
+          fullWidth
+          autoFocus
+          color="primary"
+          margin="normal"
+          variant="outlined"
+          required
+        />
+
+        <TextField
+          type="text"
+          label="nombre y apellido"
+          name="nombre y apellido"
+          onChange={dataValidation}
+          error={errorData}
+          helperText={legendData}
+          fullWidth
+          autoFocus
+          color="primary"
+          margin="normal"
+          variant="outlined"
+          required
+        />
+
+        <TextField
+          label="email"
+          name="email"
+          onChange={emailValidation}
+          error={errorEmail}
+          helperText={legendEmail}
+          fullWidth
+          autoFocus
+          color="primary"
+          margin="normal"
+          variant="outlined"
+          required
+        />
+
+        <TextField
+          label="password"
+          type="password"
+          onChange={passwordValidation}
+          error={errorPassword}
+          helperText={legendPasword}
+          fullWidth
+          autoFocus
+          color="primary"
+          margin="normal"
+          variant="outlined"
+          required
+        />
+
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={handleLogin}
+        >
+          Registrarse
+        </Button>
       </form>
     </div>
   );
 };
 
 export default RegisterForm;
+
